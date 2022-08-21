0b36772af5ec3caa628a10eadc698223cde19dc0
Capwell <proninc@yandex.ru>
2022-08-21 15:05:22 +0400

    Update forms.py

--- deals/forms.py
+++ deals/forms.py
@@ -17,9 +17,9 @@ class TaskCreateForm(forms.ModelForm):
     def clean_slug(self):
         """Обрабатывает случай, если slug не уникален."""
         cleaned_data = super().clean()
-        slug = cleaned_data['slug']
+        slug = cleaned_data('slug')
         if not slug:
-            title = cleaned_data['slug']
+            title = cleaned_data('slug')
             slug = slugify(title)[:100]
         if Task.objects.filter(slug=slug).exists():
             raise ValidationError(f'Адрес "{slug}" уже существует, '
