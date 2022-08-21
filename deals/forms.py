39d515c32c7600775430d5ca8d1656682a5658ef
mav1984 <mav1984@list.ru>
2022-08-21 13:54:01 +0300

    fix bug with title-slug convert

--- deals/forms.py
+++ deals/forms.py
@@ -7,6 +7,7 @@ from .models import Task
 
 class TaskCreateForm(forms.ModelForm):
     """Форма для создания задания."""
+
     class Meta:
         model = Task
         # Магия Джанго: через '__all__' создаётся форма из всех полей модели
@@ -17,9 +18,9 @@ class TaskCreateForm(forms.ModelForm):
     def clean_slug(self):
         """Обрабатывает случай, если slug не уникален."""
         cleaned_data = super().clean()
-        slug = cleaned_data['slug']
+        slug = cleaned_data.get('slug')
         if not slug:
-            title = cleaned_data['slug']
+            title = cleaned_data.get('title')
             slug = slugify(title)[:100]
         if Task.objects.filter(slug=slug).exists():
             raise ValidationError(f'Адрес "{slug}" уже существует, '
